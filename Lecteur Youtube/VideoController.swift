6ceb68959ae415828c972a1544e44463d1bb480b
rkorimba <korimbacus.riad@gmail.com>
2018-04-19 17:22:44 +0200

    Finalisation avec vidéo chargeable

--- Lecteur Youtube/VideoController.swift
+++ Lecteur Youtube/VideoController.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
 class VideoController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     var chanson: Chanson?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
         if chanson != nil {
-            title = chanson?.titre
+            chargerVideo(chanson: chanson!)
+        }
+    }
+    
+    func chargerVideo(chanson: Chanson) {
+        if let url = URL(string: chanson.videoUrl) {
+            let requete = URLRequest(url: url)
+            webView.load(requete)
         }
     }
 }
