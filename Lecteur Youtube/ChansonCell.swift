a3ff5511f9bc8ff64059ed6ce59a83d036f82d33
rkorimba <korimbacus.riad@gmail.com>
2018-04-19 16:57:45 +0200

    Télécharger des images depuis internet

--- Lecteur Youtube/ChansonCell.swift
+++ Lecteur Youtube/ChansonCell.swift
@@ -25,10 +25,26 @@ class ChansonCell: UITableViewCell {
 
     func creerCell(_ chanson: Chanson) {
         self.chanson = chanson
-        
+        telechargerImage()
         let attributed = NSMutableAttributedString(string: self.chanson.titre, attributes: [.font: UIFont.boldSystemFont(ofSize: 20), .foregroundColor: UIColor.black])
         let secondeLigne = NSAttributedString(string: "\n" + self.chanson.artiste, attributes: [.font: UIFont.italicSystemFont(ofSize: 20), .foregroundColor: UIColor.darkGray])
         attributed.append(secondeLigne)
         artisteEtTitreLabel.attributedText = attributed
+    }
+    
+    func telechargerImage() {
+        miniature.image = #imageLiteral(resourceName: "logo")
+        
+        if let url = URL(string: self.chanson.miniatureUrl) {
+            let session = URLSession.shared
+            let task = session.dataTask(with: url, completionHandler: { (data, reponse, error) in
+                if let imageData = data, let image = UIImage(data: imageData) {
+                    DispatchQueue.main.async {
+                        self.miniature.image = image
+                    }
+                }
+            })
+            task.resume()
+        }
     }
 }
--- Lecteur Youtube/TableauController.swift
+++ Lecteur Youtube/TableauController.swift
@@ -21,6 +21,7 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
         tableView.delegate = self
         tableView.dataSource = self
         ajouterChanson()
+        title = "Mes Vidéos Préférées"
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,7 +39,7 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 170
+        return 130
     }
     
     func ajouterChanson() {
